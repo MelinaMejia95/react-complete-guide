f27963cdb5d88c92093510368fc3447ead38b149
melinamejia95 <melinamejia95@gmail.com>
2018-11-20 20:07:01 -0500

    Working with errors - Example

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,12 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
+    
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong')
+    }
 
     return (
         <div className={classes.Person}>
@@ -11,6 +17,6 @@ const person = (props) => {
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
-}
+};
 
 export default person;
