31c334a079e26b692a54b73e9474583927f1a708
melinamejia95 <melinamejia95@gmail.com>
2018-11-27 20:49:34 -0500

    Persons and Cockpit component configured

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
@@ -56,45 +57,23 @@ class App extends Component {
   render() {
 
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return  <Person click={() => this.deletePersonHandler(index)} 
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}  
-        </div> 
-      );
-      
-      btnClass = classes.Red;
-
-    }
-
-    let assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); //classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); //classes = ['red', blod']
+      persons = <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}/>
     }
 
     return (
-        <div className={classes.App}>
-          <h1>Hi!</h1>
-          <p className={assignedClasses.join(' ')}>This is really working!</p>
-          {/* <button 
-            style={style} 
-            onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> */} {/* Try not to use it, it can be inefficient  */}
-          <button className={btnClass} onClick={this.tooglePersonHandler}>Toogle Person</button>
-        {persons}
+        <div className={classes.App}>  
+          <Cockpit 
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.tooglePersonHandler}/>
+          {persons}
         </div>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'HI!'))
   }
 }
 
