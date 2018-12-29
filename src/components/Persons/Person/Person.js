0f76418cf1e9764581c0932808a66ee691d029fb
melinamejia95 <melinamejia95@gmail.com>
2018-12-29 15:46:12 -0500

    Using Aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -23,11 +23,17 @@ class Person extends Component {
         console.log('[Person.js] Inside render()');
         return (
             <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        )
+
+        /* return [
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </div>
-        )
+        ] */
     }
 }
 
